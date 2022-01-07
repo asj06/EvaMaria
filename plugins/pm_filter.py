a8805bbf99d39327e9ccbbbe46b4f316da82f310
Amogh SJ <86942531+asj06@users.noreply.github.com>
2022-01-07 14:16:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -603,13 +603,15 @@ async def auto_filter(client, msg, spoll=False):
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"🗓 1/{round(int(total_results)/10)}",callback_data="pages"), InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_{req}_{key}_{offset}")
-        ,InlineKeyboardButton("♻️ SF Lɪɴᴋs ♻️", url="https://t.me/sflinks")])
+        ])
         btn.append(
             [InlineKeyboardButton("♻️ SF Lɪɴᴋs ♻️", url="https://t.me/sflinks")])
     else:
         btn.append(
-            [InlineKeyboardButton(text="🗓 1/1",callback_data="pages"),InlineKeyboardButton("♻️ SF Lɪɴᴋs ♻️", url="https://t.me/sflinks")]
+            [InlineKeyboardButton(text="🗓 1/1",callback_data="pages")]
         )
+        btn.append(
+            [InlineKeyboardButton("♻️ SF Lɪɴᴋs ♻️", url="https://t.me/sflinks")])
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
     if imdb:
         cap = IMDB_TEMPLATE.format(
