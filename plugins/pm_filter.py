d5a5f0c1e465efedac953fdd7aa1fcc3a72fe6b3
Amogh SJ <86942531+asj06@users.noreply.github.com>
2022-01-08 08:53:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -341,7 +341,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if not files_:
             return await query.answer('No such file exist.')
         files = files_[0]
-       ''' reply_markup = get_reply_markup(query=string)'''
         title = files.file_name
         button = [
          [
